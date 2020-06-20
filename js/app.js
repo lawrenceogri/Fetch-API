48493ad2d931b49ba3c7f21dc7a8ff24928b2ec8
Lawrence Ogri <lawrenceogri@yahoo.com>
2020-06-20 12:03:43 +0100

    Add image to page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // FETCH FUNCTIONS
-fetch('https://dog.ceo/api/breeds/image/random/3')
+fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(data => console.log(data.message))
+    .then(data => generateImage(data.message))
+
+// HELPER FUNCTION
+function generateImage(data){
+    const html = `
+        <img src='${data}' alt>
+        <p>Click to view images of </p>
+    `;
+    card.innerHTML = html;
+    console.log(card.innerHTML);
+}
