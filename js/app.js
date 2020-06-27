649467e10193461846be68143be1c8baceaed20f
Lawrence Ogri <lawrenceogri@yahoo.com>
2020-06-27 13:04:34 +0100

    Add reusable fetch function to app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
-const select = document.getElementById('Breeds');
+const select = document.getElementById('breeds');
 const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // FETCH FUNCTIONS
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
+function fetchData(url){
+    return fetch(url)
+    .then(res => console.log(res))
+        .then(res => res.json())
+        .catch(error => console.log("There was an error", error))
+
+}
+
+fetchData('https://dog.ceo/api/breeds/list')
+    .then(data => generateOptions(data.message))
+
+fetchData('https://dog.ceo/api/breeds/image/random')
     .then(data => generateImage(data.message))
 
 // HELPER FUNCTION
+function generateOptions(data){
+    const options = data.map(item => `
+        <option value='${item}'>${item}</option>
+    `).join('');
+    select.innerHTML = options;
+}
+
 function generateImage(data){
     const html = `
         <img src='${data}' alt>
-        <p>Click to view images of </p>
+        <p>Click to view images of ${select.value}</p>
     `;
     card.innerHTML = html;
-    console.log(card.innerHTML);
 }
+
+function fetchBreedImage(){
+    const breed = select.value;
+    const img = card.querySelector('img');
+    const p = card.querySelector('p');
+
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(data => {
+            img.src = data.message;
+            img.alt = breed;
+            p.textContent = `Click to view more ${breed}s`
+        })
+}
+
+// EVENT LISTENER
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
